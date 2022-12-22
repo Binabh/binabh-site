90a61efe302d497792e1712c890151934f5a346c
Binabh <binabhdevkota@gmail.com>
2022-12-22 18:08:41 +0545

    edit config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["research.dasmalab.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
